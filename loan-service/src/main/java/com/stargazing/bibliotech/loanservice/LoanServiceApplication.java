5c2b745431d14581d02df553dfe6e01fa903faa6
huderdcv <huder.delacruz.27@unsch.edu.pe>
2026-08-27 16:41:34 -0500

    chore(BIB-11): update dependencies and configure global clock bean

--- loan-service/src/main/java/com/stargazing/bibliotech/loanservice/LoanServiceApplication.java
+++ loan-service/src/main/java/com/stargazing/bibliotech/loanservice/LoanServiceApplication.java
@@ -2,12 +2,14 @@ package com.stargazing.bibliotech.loanservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class LoanServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoanServiceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(LoanServiceApplication.class, args);
+  }
 
 }
